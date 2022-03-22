e24c01a7f87dade5ee0bdfc761168c8c5a9953e6
wtsiwon <siwon4727@gmail.com>
2022-03-22 20:44:23 +0900

    3월22일

--- Corona/Assets/Scripts/Bactria.cs
+++ Corona/Assets/Scripts/Bactria.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class Bactria : Enemy
 {
     protected override void Attack() { return; }
--- Corona/Assets/Scripts/GameManager.cs
+++ Corona/Assets/Scripts/GameManager.cs
@@ -1,29 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public int Health
+    private static GameManager instance;
+
+    public static GameManager Instance
     {
         get
         {
-            return Health;
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+            }
+            return instance;
         }
+    }
+
+    [SerializeField] private int stageNum;
+    private float health;
+
+    public float Health
+    {
+        get => health;
         set
         {
-
+            health = value;
+            healthText.text = $"체력 게이지 : {health}%";
         }
+    }
+    private float pain;
+
+    public float Pain
+    {
+        get => pain;
+        set
+        {
+            pain = value;
+            painText.text = $"고통 게이지 : {pain}%";
+        }
+    }
+    private int score;
+
+    public int Score
+    {
+        get => score;
+        set
+        {
+            score = value;
+            scoreText.text = $"점수 : {score}";
+        }
+
+    }
+
+    [SerializeField] private Text healthText;
+    [SerializeField] private Text painText;
+    [SerializeField] private Text scoreText;
+    //[SerializeField] private SpawnManager spawnManager;
+
+    private void Start()
+    {
+        Health = 100;
+        Pain = stageNum == 1 ? 10 : 30;
+
+        if(stageNum == 1)
+        {
+            Score = 0;
+        }
+        ReadEnemyData(stageNum);
+    }
+
+    private void ReadEnemyData(int stageNum)
+    {
+        spawnManager.ReadEnemyData(Resources.Load<TextAsset>($"Stage{stageNum}_EnemyData").text);
     }
 }
